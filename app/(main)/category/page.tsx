a50be7f5d622150c7a403919cded7040c748bec5
Sangwon Lee <buddle@Sangwonui-MacBookAir.local>
2024-08-21 19:09:40 +0900

    fix: 21일 백업 커밋

---
+++ app/(main)/category/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainLayout from '@/app/_layout/MainLayout'
+
+const CategoryPage = () => {
+  return (
+    <MainLayout></MainLayout>
+  )
+}
+
+export default CategoryPage
---
+++ app/_layout/MainLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ReactNode } from 'react'
+
+type MainLayoutProps = {
+    children?: ReactNode
+}
+
+const MainLayout = (props: MainLayoutProps) => {
+    const { children } = props
+
+  return (
+     <main className="flex-1 overflow-y-auto h-full px-7 py-4">
+        {children}
+     </main>
+  )
+}
+
+export default MainLayout
